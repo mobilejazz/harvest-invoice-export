34538dc3c6d08dedec072a2019d2b3f2a64aa714
Jordi Giménez <jordi@mobilejazz.com>
2021-09-06 14:23:48 +0200

    using harvest api v2

--- README.md
+++ README.md
@@ -8,23 +8,21 @@ Install like this:
 
 Use like this:
 
-	mkdir invoices
-    harvest-invoice-export -u someone@mobilejazz.com -p s00p3rs3cr31 -s mobilejazz -o invoices -f 20150101 -t 20150331
+    mkdir invoices
+    harvest-invoice-export -k 1234.abababbkks33cret -a 12345 -o invoices -f 20150101 -t 20150331
 
 Full usage instructions:
 ```
-  Usage: harvest-invoice-export [options]
+Usage: harvest-invoice-export [options]
 
-  Options:
-
-    -h, --help                   output usage information
-    -V, --version                output the version number
-    -o, --out <dir>              Output directory. Must exist.
-    -u, --email <email>          Harvest email address.
-    -p, --password <password>    Harvest password.
-    -s, --subdomain <subdomain>  Harvest subdomain.
-    -f, --from <YYYYMMDD>        Starting issue date (inclusive)
-    -t, --to <YYYYMMDD>          Ending issue date (inclusive)
+Options:
+  -V, --version                      output the version number
+  -o, --out <dir>                    Output directory. Must exist.
+  -k, --access-token <access-token>  Harvest personal access token (https://id.getharvest.com/developers).
+  -a, --account-id <account-id>      Harvest account ID (https://id.getharvest.com/developers).
+  -f, --from <YYYYMMDD>              Starting issue date (inclusive)
+  -t, --to <YYYYMMDD>                Ending issue date (inclusive)
+  -h, --help                         display help for command
 ```
 
 Note from the author: this is my first Javascript in ages. Please don't judge me by this code :)
--- harvest-invoice-export.js
+++ harvest-invoice-export.js
@@ -2,52 +2,106 @@
 var program = require('commander');
 var fs = require('fs');
 var assert = require('assert');
-
-program
-  .version('0.1.0')
-  .option('-o, --out <dir>', 'Output directory. Must exist.')
-  .option('-u, --email <email>', 'Harvest email address.')
-  .option('-p, --password <password>', 'Harvest password.')
-  .option('-s, --subdomain <subdomain>', 'Harvest subdomain.')
-  .option('-f, --from <YYYYMMDD>', 'Starting issue date (inclusive)')
-  .option('-t, --to <YYYYMMDD>', 'Ending issue date (inclusive)')
-  .parse(process.argv);
-
-assert.equal(typeof (program.email), 'string', "Please specify e-mail address");
-assert.equal(typeof (program.password), 'string', "Please specify password");
-assert.equal(typeof (program.subdomain), 'string', "Please specify subdomain");
-assert.equal(typeof (program.from), 'string', "Please specify from");
-assert.ok(/^\d{8}$/.test(program.from), "Please specify from in format YYYYMMDD");
-assert.equal(typeof (program.to), 'string', "Please specify to");
-assert.ok(/^\d{8}$/.test(program.to), "Please specify to in format YYYYMMDD");
-assert.equal(typeof (program.out), 'string', "Please specify directory");
-assert.ok(fs.lstatSync(program.out).isDirectory(), "Output specified is not a directory");
-
-var Harvest = require('harvest'),
-    harvest = new Harvest({
-        subdomain: program.subdomain,
-        email: program.email,
-        password: program.password
-    });
+const https = require("https");
 var sanitize = require("sanitize-filename");
 
-getInvoices(function(err, invoices) {
-    if (err) throw JSON.stringify(err);
-    printInvoiceSummary(invoices);
-    downloadInvoicesToDisk(invoices);
+program
+  .version('2.0.0')
+  .requiredOption('-o, --out <dir>', 'Output directory. Must exist.')
+  .requiredOption('-k, --access-token <access-token>', 'Harvest personal access token (https://id.getharvest.com/developers).')
+  .requiredOption('-a, --account-id <account-id>', 'Harvest account ID (https://id.getharvest.com/developers).')
+  .requiredOption('-f, --from <YYYYMMDD>', 'Starting issue date (inclusive)')
+  .requiredOption('-t, --to <YYYYMMDD>', 'Ending issue date (inclusive)')
+  .parse(process.argv);
+
+const options = program.opts();
+assert.equal(typeof (options.accessToken), 'string', "Please specify your access token");
+assert.equal(typeof (options.accountId), 'string', "Please specify your account id");
+assert.equal(typeof (options.from), 'string', "Please specify from");
+assert.ok(/^\d{8}$/.test(options.from), "Please specify from in format YYYYMMDD");
+assert.equal(typeof (options.to), 'string', "Please specify to");
+assert.ok(/^\d{8}$/.test(options.to), "Please specify to in format YYYYMMDD");
+assert.equal(typeof (options.out), 'string', "Please specify directory");
+assert.ok(fs.lstatSync(options.out).isDirectory(), "Output specified is not a directory");
+
+getDomain(options.accessToken, options.accountId).then(domain => {
+	getInvoices(options.from, options.to, options.accessToken, options.accountId).then(invoices => {
+		printInvoiceSummary(invoices);
+		downloadInvoicesToDisk(domain, invoices, options.out);
+	});	
 });
 
-function getInvoices(cb, knownInvoices, pageToRequest) {
-	var pageToRequest = pageToRequest || 1;
-	harvest.Invoices.client.get('/invoices', {"from":program.from, "to":program.to, "page": pageToRequest}, function(err, invoices) {
-		if (err) {
-			cb(err);
-			return;
-		}
-		knownInvoices = knownInvoices || [];
-		knownInvoices = knownInvoices.concat(invoices);
-		if (invoices.length == 50) getInvoices(cb, knownInvoices, pageToRequest+1); // next page
-		else cb(null, knownInvoices); // all pages done
+function getInvoices(from, to, accessToken, accountId) {
+	return new Promise((resolve, reject) => {
+		https.get({
+			protocol: "https:",
+			host: "api.harvestapp.com",
+			path: "/v2/invoices?" + new URLSearchParams({
+				"from": from, 
+				"to": to, 
+				"page": "1",
+				"per_page": "100",
+			}).toString(),
+			headers: {
+			  "User-Agent": "Mobile Jazz Harvest invoice exporter",
+			  "Authorization": "Bearer " + accessToken,
+			  "Harvest-Account-Id": accountId,
+			}
+		}, (res) => {
+			const { statusCode } = res;
+			if (statusCode !== 200) throw `Request failed with status: ${statusCode}`;
+			
+			res.setEncoding('utf8');
+			let rawData = '';
+			res.on('data', (chunk) => { rawData += chunk; });
+			res.on('end', () => {
+				let parsedData;
+				try {
+					parsedData = JSON.parse(rawData);
+					if (parsedData.total_pages > 1) {
+						throw 'not implemented: more than 100 invoices in time frame';
+					}
+				} catch (e) {
+					reject(e);
+				}
+				resolve(parsedData.invoices);
+			});
+		}).on('error', (e) => {
+			reject(e);
+		});
+	});
+}
+
+function getDomain(accessToken, accountId) {
+	return new Promise((resolve, reject) => {
+		https.get({
+			protocol: "https:",
+			host: "api.harvestapp.com",
+			path: "/v2/company",
+			headers: {
+			  "User-Agent": "Mobile Jazz Harvest invoice exporter",
+			  "Authorization": "Bearer " + accessToken,
+			  "Harvest-Account-Id": accountId,
+			}
+		}, (res) => {
+			const { statusCode } = res;
+			if (statusCode !== 200) throw `Request failed with status: ${statusCode}`;
+			
+			res.setEncoding('utf8');
+			let rawData = '';
+			res.on('data', (chunk) => { rawData += chunk; });
+			res.on('end', () => {
+				let parsedData;
+				try {
+					parsedData = JSON.parse(rawData);
+				} catch (e) {
+					reject(e);
+				}
+				resolve(parsedData.full_domain);
+			});
+		}).on('error', (e) => {
+			reject(e);
+		});
 	});
 }
 
@@ -56,13 +110,11 @@ function printInvoiceSummary(invoices) {
 	var headers = ["Invoice Date", "Serial No", "Company", "Base", "IVA", "Total", "Currency"];
 
 	// transform
-	var json = invoices.reverse().map(function (inv) {
-		var invoice = inv.invoices; // weird, I dont know why they do that
-
+	var json = invoices.reverse().map(function (invoice) {
 		return {
-			"Invoice Date": invoice.issued_at,
+			"Invoice Date": invoice.issue_date,
 			"Serial No": invoice.number,
-			"Company": invoice.client_name,
+			"Company": invoice.client.name,
 			"Base": invoice.amount - invoice.tax_amount - invoice.tax2_amount,
 			"IVA": invoice.tax_amount + invoice.tax2_amount,
 			"Total": invoice.amount,
@@ -77,22 +129,18 @@ function printInvoiceSummary(invoices) {
 	});
 }
 
-function downloadInvoicesToDisk(invoices) {
-	invoices.forEach(function (inv) {
-		var invoice = inv.invoices; // weird, I dont know why they do that
-		var filename = program.out + '/' + sanitize(invoice.issued_at.replace(/-/g, "_") + " " + invoice.number + " " + invoice.client_name + ".pdf");
-		var url = "https://" + program.subdomain + ".harvestapp.com/client/invoices/" + invoice.client_key + ".pdf"
+function downloadInvoicesToDisk(domain, invoices, outFolder) {
+	invoices.forEach(function (invoice) {
+		var filename = outFolder + '/' + sanitize(invoice.issue_date.replace(/-/g, "_") + " " + invoice.number + " " + invoice.client.name + ".pdf");
+		var url = "https://" + domain + "/client/invoices/" + invoice.client_key + ".pdf"
 		downloadFile(url, filename);
 	});
 }
 
 function downloadFile(url, filename) {
-		var https = require('https');
-		var fs = require('fs');
-
-		var file = fs.createWriteStream(filename);
-		var request = https.get(url, function(response) {
-		  response.pipe(file);
-		});
+	const file = fs.createWriteStream(filename);
+	https.get(url, function(response) {
+		response.pipe(file);
+	});
 }
 
